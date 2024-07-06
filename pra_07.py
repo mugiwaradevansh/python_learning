dd60176bc9e9841a1dfaf694b46bdad6931ab0ff
devansh <dsppatel.149@gmail.com>
2024-07-06 22:16:00 +0530

    7

--- pra_07.py
+++ pra_07.py
@@ -3,9 +3,9 @@ for i in colors:
     print(i)
     if i == "green":
         print("\n")
-    # print(" /next color")
-    # print("     /end of loop")
-    # print("         /after break")
+    print("  /next color")
+    print(" /end of loop")
+    print("/after break")
 
 name = "Devansh"
 for lp in name:
@@ -14,3 +14,7 @@ for lp in name:
         print(f"{lp} - It's my fav letter")
         continue
 
+for k in range(5):
+    print(k)
+    if k == 4:
+        print("I'm skipping 4")
